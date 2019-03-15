5439eba8b279c53402565882921bb0b36c7a2295
HexHackGames <48191099+HexHackGames@users.noreply.github.com>
2019-03-15 02:03:12 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,18 +90,6 @@
 				$body.removeClass('is-preload');
 			}, 100);
 		});
-	
-		$(window).load(function () {
-		    $(".trigger_popup_fricc").click(function(){
-		       $('.hover_bkgr_fricc').show();
-		    });
-		    $('.hover_bkgr_fricc').click(function(){
-			$('.hover_bkgr_fricc').hide();
-		    });
-		    $('.popupCloseButton').click(function(){
-			$('.hover_bkgr_fricc').hide();
-		    });
-		});
 
 	// Tweaks/fixes.
 
