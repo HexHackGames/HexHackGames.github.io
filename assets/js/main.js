ca853706e67bb7b02902245214fc8b399f536235
HexHackGames <48191099+HexHackGames@users.noreply.github.com>
2019-03-15 01:59:27 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -757,3 +757,15 @@
 					});
 
 })(jQuery);
+
+$(window).load(function () {
+    $(".trigger_popup_fricc").click(function(){
+       $('.hover_bkgr_fricc').show();
+    });
+    $('.hover_bkgr_fricc').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+    $('.popupCloseButton').click(function(){
+        $('.hover_bkgr_fricc').hide();
+    });
+});
